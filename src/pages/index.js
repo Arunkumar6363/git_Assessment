6a192492859840e795b2bec20c9b8a31fc68b70b
Arunkumar <arunkumarp@piraiinfo.com>
2025-06-17 15:04:05 +0530

    Stash commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
 
 
 export default function Home() {
+  const [count , setCount ] = useState(0)
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className=" flex flex-col gap-4 justify-center items-center">
+      <h1 className="text-4xl">Home Page</h1>
+      <button 
+      className=" border border-gray-500 w-9 text-3xl "
+      onClick={() => setCount(count + 1) } >+</button>
+      <p className="text-2xl"> result :{count}</p>
+      <button  className="border border-gray-500 w-9 text-3xl "
+      onClick={() => setCount(count - 1)}> - </button>
     </div>
   );
 }
